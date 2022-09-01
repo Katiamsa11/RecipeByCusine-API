f633dc547b580992d5a1269989145e8390810545
katia santos <katiamsa@gmail.com>
2022-08-31 23:42:30 -0400

    added yelp database and finalized backend api

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 9000;
 const cors = require("cors");
 const recipesRoutes = require("./routes/recipes.js");
 const axios = require("axios");
+const API_URL = "https://api.yelp.com/v3/businesses/search?location=toronto";
+const API_KEY =
+  "SYFPNjTnFMkHD_xE5QPT3cwtgrrHenIGPkwgjhdxoNBkCvtzHAB99d1tTaOnIM_Lfp8WNv4cm-lViq-7wWvJLcEM3kZotncG41LyAvUrcoZX3uAVNflqhoyFJL4PY3Yx";
+axios.defaults.headers.common = {
+  Authorization: "Bearer " + API_KEY,
+};
 
 // added middleware to allow any origin (front-end) to interact with API
 app.use(cors());
@@ -15,6 +21,18 @@ app.use(express.json());
 app.use("/images", express.static("./images"));
 
 app.use("/recipes", recipesRoutes);
+
+app.get("/restaurants", (req, res) => {
+  axios
+    .get(API_URL)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error.data);
+    });
+});
 
 //listening on port 8000
 app.listen(PORT, () => {
